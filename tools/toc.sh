e190e3b3e035539373403f1c052c01c37f45bce1
Brad Campbell <bradjc5@gmail.com>
2021-02-01 19:20:48 -0500

    tools: update toc.sh
    Use Rust md-toc rather than an NPM tool.

--- tools/toc.sh
+++ tools/toc.sh
@@ -4,15 +4,26 @@
 # SPDX-License-Identifier: Apache-2.0 OR MIT
 # Copyright Tock Contributors 2023.
 
-# Run `markdown-toc` on all Markdown files with tables of contents
-# to see if any of them need an updated TOC.
+# Runs `md-toc` (from the [markdown-toc](https://crates.io/crates/markdown-toc)
+# crate) on all Markdown files with tables of contents to see if any of them
+# need an updated TOC.
 #
-# As a side effect, if you run this locally it will generate
-# all needed TOC and update the markdown documents.
+# As a side effect, if you run this locally it will generate all needed TOC and
+# update the markdown documents.
+#
+# To add a table of contents to a markdown file, add the tags:
+#
+#     <!-- toc -->
+#     <!-- tocstop -->
+#
+# to the markdown file where you want the table of contents and run this script.
 #
 # Author: Brad Campbell <bradjc5@gmail.com>
 
 let ERROR=0
+
+# Make sure the `markdown-toc` tool is installed.
+cargo install markdown-toc
 
 # Find all markdown files
 for f in $(find . -path ./node_modules -prune -false -o -name "*.md"); do
@@ -24,7 +35,14 @@ for f in $(find . -path ./node_modules -prune -false -o -name "*.md"); do
 	if [[ $rc == 0 ]]; then
 		# Try running the TOC tool and see if anything changes
 		before=`cat $f`
-		markdown-toc -i $f
+		tableofcontents=`md-toc $f --bullet "-" --indent 2 --min-depth 1 --no-header`
+		replace='<!-- toc -->
+
+<!-- Build table of contents with tools\/toc.sh -->
+'$tableofcontents'
+
+<!-- tocstop -->'
+		perl -i -p0e "s/\<\!-- toc --\>.*\<\!-- tocstop --\>/$replace/gs" $f
 		after=`cat $f`
 
 		if [[ "$before" != "$after" ]]; then
